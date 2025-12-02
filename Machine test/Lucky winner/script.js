369fd1ea24743e41b93ddab580b346dcf3aeed66
Vyshnav <vyshnav2004sk@gmail.com>
2025-12-02 16:41:00 +0530

    qn 2

--- Machine test/Lucky winner/script.js
+++ Machine test/Lucky winner/script.js
@@ -1,17 +1,16 @@
+function selectWinner() {
 const lotteryTickets = [
     1001, 1002, 1003, 1004, 1005, 1111, 
     2050, 2065, 3099, 3333, 4100, 5555, 
     7777, 8888, 9999, 5432, 1234, 4321
 ];
 
-const drawButton = document.getElementById('drawBtn');
 const pageDisplay = document.getElementById('result-display');
 const modalDisplay = document.getElementById('modal-winner-number');
 
 const winnerModalElement = document.getElementById('winnerModal');
 const winnerModal = new bootstrap.Modal(winnerModalElement);
 
-function selectWinner() {
 
     const randomIndex = Math.floor(Math.random() * lotteryTickets.length);
     const winner = lotteryTickets[randomIndex];
@@ -25,6 +24,3 @@ function selectWinner() {
     winnerModal.show();
 }
 
-if (drawButton) {
-    drawButton.addEventListener('click', selectWinner);
-}
